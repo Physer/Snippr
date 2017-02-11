396fc875a4a345350fb733ad8b2c2f9f9e8d8b9f
Alex Schouls <alex_schouls@live.com>
2017-02-11 14:21:13 +0100

    Removed unused/unnecessary nuget packages from projects

--- src/Snippr.API/Global.asax.cs
+++ src/Snippr.API/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Http;
-using System.Web.Mvc;
 using AutoMapper;
 using Snippr.API.App_Start;
 using Snippr.Services.Mappers;
@@ -10,9 +9,7 @@ namespace Snippr.API
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             ContainerConfig.Register();
             ConfigureMappings();
         }
