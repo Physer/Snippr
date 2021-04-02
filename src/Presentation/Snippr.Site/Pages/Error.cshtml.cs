6804c7e9979b1b5539d62e7c314b9e69ab429a80
Alex Schouls <alex_schouls@live.com>
2021-04-02 20:10:09 +0200

    Restructured folder structure

--- src/Presentation/Snippr.Site/Pages/Error.cshtml.cs
+++ src/Presentation/Snippr.Site/Pages/Error.cshtml.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Snippr.Site.Pages
 {
--- src/Presentation/Snippr.Site/Pages/Privacy.cshtml.cs
+++ src/Presentation/Snippr.Site/Pages/Privacy.cshtml.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Snippr.Site.Pages
 {
