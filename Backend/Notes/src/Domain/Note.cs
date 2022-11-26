1ab6dc65ea0607f77c78c20bbd24f53e719a40b9
Alex Schouls <alex_schouls@live.com>
2022-11-26 21:33:53 +0100

    [Notes] Added Note domain model

---
+++ Backend/Notes/src/Domain/Note.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain;
+
+public class Note
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public string? Language { get; set; }
+
+    public Note(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public Note(string name, string description, string language) : this(name, description) => Language = language;
+}
