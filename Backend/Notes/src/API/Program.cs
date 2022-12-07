82779cb951232123c21dfcad2796a47bc8895c70
Alex Schouls <alex_schouls@live.com>
2022-12-07 13:25:17 +0100

    [Notes] Added GraphQL server

--- Backend/Notes/src/API/Program.cs
+++ Backend/Notes/src/API/Program.cs
@@ -1,6 +1,11 @@
+using API;
+
 var builder = WebApplication.CreateBuilder(args);
+builder
+    .Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGraphQL();
 app.Run();
---
+++ Backend/Notes/src/API/Query.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace API;
+
+public class Query
+{
+    public Note GetNote() => new("Mocked note", "This is a static note description");
+}
