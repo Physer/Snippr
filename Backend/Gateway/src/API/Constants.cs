3c516ed815978160f21f067ad137b15eea0063c1
Alex Schouls <alex_schouls@live.com>
2022-12-07 13:54:03 +0100

    [Gateway] Added Federation support

---
+++ Backend/Gateway/src/API/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace API;
+
+public static class Constants
+{
+    public static class Schemas
+    {
+        public const string Notes = "Notes";
+    }
+}
--- Backend/Gateway/src/API/Program.cs
+++ Backend/Gateway/src/API/Program.cs
@@ -1,6 +1,16 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddHttpClient("Notes", config => config.BaseAddress = new Uri(configuration["Subgraphs:Notes"] ?? string.Empty));
+
+builder
+    .Services
+    .AddSingleton(ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis") ?? string.Empty))
+    .AddGraphQLServer()
+    .AddRemoteSchemasFromRedis("Snippr", factory => factory.GetRequiredService<ConnectionMultiplexer>());
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGraphQL();
 app.Run();
