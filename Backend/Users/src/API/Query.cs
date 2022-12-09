723552f8682d09efe4cf7a7ddcbf8e82a1605dcd
Alex Schouls <alex.schouls@valtech.com>
2022-12-09 09:58:42 +0100

    [Users] Added User model and Query

---
+++ Backend/Users/src/API/Query.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace API;
+
+public class Query
+{
+    public User GetUser => new("mock_user_1337", "mock_user@snippr.com");
+}
---
+++ Backend/Users/src/Domain/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public User(string username, string email)
+    {
+        Username = username;
+        Email = email;
+    }
+}
