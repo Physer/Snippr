c05a988e8144f06fa98354160ec301f095c60cd8
Alex Schouls <alex_schouls@live.com>
2022-12-18 12:59:56 +0100

    [Site] Continue work for nav bar

--- Site/app/components/navigation.tsx
+++ Site/app/components/navigation.tsx
@@ -1,22 +1,17 @@
 export default function Navigation() {
   return (
-    <div className="relative h-16 bg-slate-200 dark:bg-slate-600 min-h-fit">
-      <div className="absolute inset-y-0 left-0">
+    <nav className="h-16 bg-slate-200 dark:bg-slate-600 min-h-fit flex items-center">
+      <div className="h-full">
         {/* Some kind of issue with Next's components for now... */}
         <a href="/">
-          <img src="/logo-no-background.png" alt="Snippr" className="h-full" />
+          <img src="/logo-no-background.png" alt="Snippr" className="h-full"/>
         </a>
       </div>
-      <div className="absolute inset-y-0 right-0 flex flex-row ">
-        <a href="#" className="h-10">
-          TODO: Settings
-          {/* <img src="/settings.png" alt="Settings" className="object-cover" /> */}
-        </a>
-        <a href="#" className="h-10">
-          TODO: Profile
-          {/* <img src="/profile.png" alt="Profile" className="h-10" /> */}
+      <div className="h-8=">
+        <a href="#">
+          <img src="/profile.png" alt="Profile" className="h-8"/>
         </a>
       </div>
-    </div>
+    </nav>
   );
 }
