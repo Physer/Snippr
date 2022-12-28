5b8f3ef63a95e4d54155870c8e012bdddc7ba915
Alex Schouls <alex.schouls@valtech.com>
2022-12-28 15:00:05 +0100

    [Gateway] Adding CORS support (temporarily allow all)

--- Gateway/src/API/Program.cs
+++ Gateway/src/API/Program.cs
@@ -3,6 +3,8 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+builder.Services.AddCors();
+
 builder.Services.AddHttpClient("Notes", config => config.BaseAddress = new Uri(configuration["Subgraphs:Notes"] ?? string.Empty));
 builder.Services.AddHttpClient("Users", config => config.BaseAddress = new Uri(configuration["Subgraphs:Users"] ?? string.Empty));
 
@@ -13,5 +15,6 @@ builder
     .AddRemoteSchemasFromRedis("Snippr", factory => factory.GetRequiredService<ConnectionMultiplexer>());
 
 var app = builder.Build();
+app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.MapGraphQL();
 app.Run();
